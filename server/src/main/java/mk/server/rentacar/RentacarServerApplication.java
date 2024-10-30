5ccd742e14eda2df9fc46722bd9256435932788d
Mark0z <markoz@interia.com>
2024-10-30 15:44:55 +0100

    first commit

---
+++ server/src/main/java/mk/server/rentacar/RentacarServerApplication.java
@@ -0,0 +1,13 @@
+package mk.server.rentacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/mk/server/rentacar/RentacarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.server.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
