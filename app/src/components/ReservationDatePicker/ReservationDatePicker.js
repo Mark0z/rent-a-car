0c57ce0639cac688444692e4c3699371e0d1dad6
Mark0z <markoz@interia.com>
2024-11-15 22:38:03 +0100

    Added ReservationDatePicker component

---
+++ app/src/components/ReservationDatePicker/ReservationDatePicker.js
@@ -0,0 +1,79 @@
+import 'components/ReservationDatePicker/ReservationDatePicker.scss';
+import { useForm } from 'react-hook-form';
+import { BRANCHES_LIST } from 'data/branches-companies';
+
+export const ReservationDatePicker = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    //temporarily
+    console.log(data);
+  };
+
+  return (
+    <form className="rent-a-car-form" onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="startDate" className="rent-a-car-form--label">
+        Data odbioru:
+      </label>
+      <input
+        className="rent-a-car-form--input"
+        type="date"
+        required
+        name="startDate"
+        {...register('startDate', { required: true })}
+      />
+      {errors.startDate && <span>This field is required</span>}
+      <label htmlFor="startAgencyName" className="rent-a-car-form--label">
+        Miejsce odbioru:
+      </label>
+      <select
+        name="startAgencyName"
+        className="rent-a-car-form--input"
+        defaultValue={'default'}
+        {...register('startAgencyName', { required: true })}>
+        <option key="default" value="" hidden>
+          Wybierz miejsce odbioru
+        </option>
+        {BRANCHES_LIST.map((value) => (
+          <option key={value} value={value}>
+            {value}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="endDate" className="rent-a-car-form--label">
+        Data zwrotu:
+      </label>
+      <input
+        className="rent-a-car-form--input"
+        type="date"
+        name="endDate"
+        required
+        {...register('endDate', { required: true })}
+      />
+      {errors.endDate && <span>This field is required</span>}
+      <label htmlFor="endAgencyName" className="rent-a-car-form--label">
+        Miejsce zwrotu:
+      </label>
+      <select
+        name="endAgencyName"
+        className="rent-a-car-form--input"
+        defaultValue={'default'}
+        {...register('endAgencyName', { required: true })}>
+        <option key="default" value="" hidden>
+          Wybierz miejsce zwrotu
+        </option>
+        {BRANCHES_LIST.map((value) => (
+          <option key={value} value={value}>
+            {value}
+          </option>
+        ))}
+      </select>
+
+      <input className="rent-a-car-form--submit" type="submit" value="Rezerwuj" />
+    </form>
+  );
+};
