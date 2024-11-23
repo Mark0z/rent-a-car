d177d01bbfd3b9a0cf047f7eb05c8a1c6c81d8d0
Mark0z <markoz@interia.com>
2024-11-23 18:35:04 +0100

    styled components - improve layout and design

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -6,29 +6,39 @@ import clsx from 'clsx';
 
 export const Header = () => {
   return (
-    <header>
+    <header className="header">
       <div className="header-content">
         <a className="logo" href="/">
           <img src={logo || ''} alt="" />
         </a>
-        <div className="contact">
-          <div className="contact-info">
-            <p className={clsx('contact-info--p', 'contact-info--p__title')}>RentDrive</p>
-            <p className="contact-info--p">ul. Równa 13</p>
-            <p className="contact-info--p">80-067 Gdańsk</p>
-            <p className={clsx('contact-info--p', 'contact-info--contact-info--p__small')}>
+        <div className="header-contact">
+          <div className="header-contact-info">
+            <p className={clsx('header-contact-info--p', 'header-contact-info--p__title')}>
+              RentDrive
+            </p>
+            <p className="header-contact-info--p">ul. Równa 13</p>
+            <p className="header-contact-info--p">80-067 Gdańsk</p>
+            <p
+              className={clsx(
+                'header-contact-info--p',
+                'header-contact-info--contact-info--p__small'
+              )}>
               PON - SOB 7 - 23
             </p>
-            <p className={clsx('contact-info--p', 'contact-info--contact-info--p__small')}>
+            <p
+              className={clsx(
+                'header-contact-info--p',
+                'header-contact-info--contact-info--p__small'
+              )}>
               NDZ 6 - 24
             </p>
           </div>
-          <div className="contact-banner">
-            <p className="contact-banner__email">
+          <div className="header-contact-banner">
+            <p className="header-contact-banner__email">
               <CiMail />
               rentdrive@rentdrive.pl
             </p>
-            <p className="contact-banner__phone">
+            <p className="header-contact-banner__phone">
               <SlPhone />
               +48 123 66 55 88
             </p>
