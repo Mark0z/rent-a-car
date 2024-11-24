38245cf65bdd33be7c789c8cdc5adfc85b524e58
Mark0z <markoz@interia.com>
2024-11-24 20:48:55 +0100

    Added TextArea input & Button

---
+++ app/src/components/inputs/button/Button.js
@@ -0,0 +1,30 @@
+import './button.scss';
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+
+export const Button = React.forwardRef(
+  ({ className, value, children, type = 'button', ...props }, ref) => {
+    return (
+      <div className="button">
+        <button
+          type={type}
+          className={clsx('button-input', className, type === 'submit' && 'button-input__submit')}
+          value={value}
+          ref={ref}
+          {...props}>
+          {children}
+        </button>
+      </div>
+    );
+  }
+);
+
+Button.displayName = 'Button';
+
+Button.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string,
+  type: PropTypes.string,
+  children: PropTypes.node
+};
