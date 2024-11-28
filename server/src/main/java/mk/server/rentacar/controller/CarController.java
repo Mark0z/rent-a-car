04e106b7f41ebe2dc1dbb056118f515916d0ec34
Mark0z <markoz@interia.com>
2024-11-28 11:45:19 +0100

    fix

--- server/src/main/java/mk/server/rentacar/controller/CarController.java
+++ server/src/main/java/mk/server/rentacar/controller/CarController.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,18 +38,7 @@ public class CarController {
 
     @GetMapping("/available")
     public List<Car> getAvailableCars(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            Date parsedStartDate = dateFormat.parse(startDate);
-            Date parsedEndDate = dateFormat.parse(endDate);
-            Timestamp timestampStartDate = new java.sql.Timestamp(parsedStartDate.getTime());
-            Timestamp timestampEndDate = new java.sql.Timestamp(parsedEndDate.getTime());
-
-            return carService.getAvailableCars(timestampStartDate, timestampEndDate);
-        } catch (Exception e) { //this generic but you can control another types of exception
-            System.out.println(e.getMessage());
-        }
-        return null;
+        return carService.getAvailableCars(startDate, endDate);
     }
 
     @PostMapping("/")
