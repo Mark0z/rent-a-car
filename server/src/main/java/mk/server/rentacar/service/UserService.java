63c97e47d11323cd80d6de448e4be112f275fc7e
Mark0z <markoz@interia.com>
2024-12-07 11:51:23 +0100

    Added logic for updating profile and changing password

--- server/src/main/java/mk/server/rentacar/service/UserService.java
+++ server/src/main/java/mk/server/rentacar/service/UserService.java
@@ -2,7 +2,6 @@ package mk.server.rentacar.service;
 
 import mk.server.rentacar.model.User;
 import mk.server.rentacar.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +9,9 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
