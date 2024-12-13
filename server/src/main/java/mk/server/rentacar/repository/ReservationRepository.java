55d9dd452b529458dd10e61abbf90d0a8f680989
Mark0z <markoz@interia.com>
2024-12-13 12:09:15 +0100

    quick fix

--- server/src/main/java/mk/server/rentacar/repository/ReservationRepository.java
+++ server/src/main/java/mk/server/rentacar/repository/ReservationRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> getReservationsByCarId(Long id);
-
     List<Reservation> getReservationsByUserId(Long id);
 }
